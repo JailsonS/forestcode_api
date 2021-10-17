0f2a91b26a1c339f99dffe52ccf92951122d6a00
Jailson Soares de Souza Filho <jailson.filho@techlead.local>
2021-10-17 20:02:45 -0300

    fix test case

--- tests/Unit/PropriedadeTest.php
+++ tests/Unit/PropriedadeTest.php
@@ -5,13 +5,17 @@ use Src\Domain\Farm\Municipality;
 use Src\Domain\Farm\Repository\FarmRepository;
 use Src\Infra\Database\EntityManagerFactory;
 
-$emFactory = new EntityManagerFactory();
-$em = $emFactory->_getEntityManager();
+
+beforeEach(function () {
+    $this->emFactory = new EntityManagerFactory();
+    $this->em = $emFactory->_getEntityManager();
+});
+
 
 test('O formato do município ao qual uma propriedade está localizada de ve ser: MUNICÍPIO - UF', function() {
 
     // arrange
-    $munRepo = $em->getRepository(Municipality::class);
+    $munRepo = $this->em->getRepository(Municipality::class);
     
     // act
     $municipio = $munRepo->findBy(['cod_ibge' => 1508308]);
